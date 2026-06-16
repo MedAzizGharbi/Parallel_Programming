7ad8c9654d79478ffaa3053aa18c977417eb5264
MohamedAzizGharbi <gharbi.aziz03@gmail.com>
2026-06-16 18:48:50 +0200

    slurm submit

---
+++ exercise5/slurm_submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=matmul_benchmark
+#SBATCH --output=/home/fd0007977/out/bench.out.%j
+#SBATCH --error=/home/fd0007977/out/bench.err.%j
+#SBATCH --time=00:15:00
+
+N_VALUES=10000000000
+THREADS_VALUES=(1 2 4 8 16 32 64)
+
+OUTPUT=/home/fd0007977/out/results.csv
+
+for T in "${THREADS_VALUES[@]}"; do
+	for run in {1..3}; do
+		./pi_par $N $T >> $OUTPUT
+	done
+done
